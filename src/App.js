032479ad337c7e2f637b43e5106003b717b98031
tnfo222 <tayforbes92@hotmail.com>
2022-08-02 18:48:53 -0400

    Added Mole Container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import MoleContainer from './Components/MoleContainer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let [score, setScore] = useState(0)
+
+    const createMoleHill = () => {
+        let hills = []
+        for (let i = 0; i < 9; i++) {
+            hills.push(
+                <MoleContainer
+                key={i}
+                setScore={setScore}
+                score={score} />
+            )
+        }
+    
+        return (
+            <div>
+              { hills }
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>React-a-Mole!</h1>
+            {score}
+            {createMoleHill()}
+        </div>
+    )
 }
 
-export default App;
+export default App
+
---
+++ src/Components/MoleContainer.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Mole from './Mole';
+
+function MoleContainer (props) {
+  let [theMole, setMole] = useState(false);
+  
+  function handleClick(e)  {
+    props.setScore(props.score + 1)
+    setMole(false)
+  }
+  let displayMole = theMole ? <Mole setScore={props.setScore} toggle={setMole} handleClick={handleClick} /> : <EmptySlot toggle={setMole}/>
+
+  return (
+    <div style={{'display': 'inline-block', 'width': '30vw'}}>
+        {displayMole}
+    </div>
+  )
+}
+
+export default MoleContainer
